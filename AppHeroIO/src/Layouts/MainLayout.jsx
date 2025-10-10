e2d25c6b8ea24bbcbb19c42032bf13da4bad2f47
rubayet2027 <u2204002@student.cuet.ac.bd>
2025-10-10 23:44:19 +0600

    Added Some Fix

--- AppHeroIO/src/Layouts/MainLayout.jsx
+++ AppHeroIO/src/Layouts/MainLayout.jsx
@@ -3,11 +3,13 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router';
 import { InstalledProvider } from '../context/InstalledContext';
+import ScrollTop from '../components/ScrollTop';
 
 const MainLayout = () => {
     return (
             <InstalledProvider>
                 <div className='flex flex-col min-h-screen'>
+                        <ScrollTop />
                         <NavBar />
                         <div className="flex-1">
                              <Outlet  />
--- AppHeroIO/src/context/useInstalled.jsx
+++ AppHeroIO/src/context/useInstalled.jsx
@@ -3,7 +3,6 @@ import InstalledContext from './InstalledContext';
 
 export const useInstalled = () => {
   const ctx = useContext(InstalledContext);
-  if (!ctx) throw new Error('useInstalled must be used within an InstalledProvider');
   return ctx;
 };
 
